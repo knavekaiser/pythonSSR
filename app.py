4de6ff7c9419cbe5227cb7253bbefb654b41d21d
knavekaiser <naeem.ahmad.9m@gmail.com>
2021-01-22 17:54:23 +0600

    suctom search path

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-print(BeautifulSoup)
+import sys
+sys.path.append('/modules')
+type(sys.path)
+
+for path in sys.path:
+    print(path)
