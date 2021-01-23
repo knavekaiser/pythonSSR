0d8111c74dfcebb76d9c05161a534b6e9eedeb0f
knavekaiser <naeem.ahmad.9m@gmail.com>
2021-01-23 13:23:15 +0600

    chromedriver_binary

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-print(BeautifulSoup)
+import chromedriver_binary  # Adds chromedriver binary to path
+
+driver = webdriver.Chrome()
+# driver.get("http://www.python.org")
+
+# options = Options()
+# options.headless = True
+# driver = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=options)
+
+driver.get("http://localhost:3001/")
+print(driver.current_url)
+driver.quit()
